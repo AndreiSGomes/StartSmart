32dc7c36676d9a98dc4f82c9c6d49dbe741c8a23
Andrei <andreisouza07@gmail.com>
2020-10-15 20:30:34 -0300

    update commit

---
+++ mapa.py
@@ -0,0 +1,20 @@
+import pygame
+
+class Cenario(pygame.sprite.Sprite):
+    def __init__(self, image):
+        super().__init__()
+        self.fundo = pygame.image.load("data/%s"% image)
+        self.image = self.fundo
+        self.rect = self.image.get_rect()
+        self.vel_fundo = 0
+
+    def velocidadeFundo(self, valor):
+        self.vel_fundo = valor
+
+    def update(self):
+        self.rect.move_ip(self.vel_fundo, 0)
+        if self.rect.right <= 900:
+            self.rect.right = 1300
+
+
+
