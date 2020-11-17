219dd551fc9abeac50c2740d98fd29ce251815c6
Kaique Sousa <63821804+ShivKSF@users.noreply.github.com>
2020-11-17 15:36:54 -0300

    Menu+Jogo

--- game.py
+++ game.py
@@ -30,95 +30,199 @@ class Game():
             self.display.fill(self.BLACK)
             self.draw_text('Obrigado por Jogar', 20, self.DISPLAY_W/2, self.DISPLAY_H/2)
             self.window.blit(self.display, (0,0))
-            #ÁREA DE ADICIONAR JOGO
-            pygame.init() # Iniciando o Pygame
+#INICIO
+            # # #  CONFIGURAÇÕES DE TELA ------------------------------------------------------------------
+            larguraTela, alturaTela = 1000, 500
+            metadeLargura = larguraTela / 2
+            metadeAltura = alturaTela / 2
+            areaTela = larguraTela * alturaTela
+            tela = pygame.display.set_mode((larguraTela, alturaTela))
+            bg = pygame.image.load("Cenario/cenario.png").convert()
+            background = pygame.transform.scale(bg, (larguraTela, alturaTela))
 
-            preto = (0, 0, 0) # Cores para facilitar
-            branco = (255, 255, 255)
+            pygame.init()
+            CLOCK = pygame.time.Clock()
+            FPS = 60
+            BLACK = (0, 0, 0)
 
-            screen = pygame.display.set_mode((890, 550), 0, 32)
+            # # # TEMPORIZADOR ----------------------------------------------------------------------------
+            timer = 0
+            tempo_segundo = 0
 
-            # define o titulo da janela
-            pygame.display.set_caption("Duck Hunt - PyGame")
+            font = pygame.font.SysFont('arial black', 30)
+            texto = font.render("Tempo: ", True, (255, 255, 255), (0, 0, 0))
+            pos_texto = texto.get_rect()
+            pos_texto.center = (66, 50)
 
-            # variáveis que armazenarão a posicao em que o mouse se encontra
-            x_pos = 0
-            y_pos = 0
+            # # # MUSICA -----------------------------------------------------------------------------------
+            pygame.mixer.music.load('Musicas/Wandering-the-Streets_Looping.mp3')
+            pygame.mixer.music.play()
+            pygame.event.wait()
 
-            # variáveis que armazenarão a posicao em que um clique no mouse foi efetuado
-            x_clique = 0
-            y_clique = 0
+            # # # PERSONAGEM ---------------------------------------------------------------------------------
+            left = [pygame.image.load(os.path.join('Personagens', 'Tras1.png')),
+                    pygame.image.load(os.path.join('Personagens', 'Tras2.png')),
+                    pygame.image.load(os.path.join('Personagens', 'Tras3.png')),
+                    pygame.image.load(os.path.join('Personagens', 'Tras4.png'))
+                    ]
+            right = [pygame.image.load(os.path.join('Personagens', 'Frente1.png')),
+                    pygame.image.load(os.path.join('Personagens', 'Frente2.png')),
+                    pygame.image.load(os.path.join('Personagens', 'Frente3.png')),
+                    pygame.image.load(os.path.join('Personagens', 'Frente4.png'))
+                    ]
 
-            # variáveis que armazenarão a posicao em que o pato se encontra
-            x_duck = 0
-            y_duck = randint(0, 450)
+            x = 100
+            y = 395
+            radius = 80
+            vel = 5
+            move_left = False
+            move_right = False
+            stepIndex = 0
 
-            pontos = 0
-            velocidade = 2
-            errou = False
+            class Hero:
+                def __init__(self, x, y):
+                    #walk
+                    self.x = x
+                    self.y = y
+                    self.velx = 5
+                    self.vely = 15
+                    self.face_right = True
+                    self.face_left = False
+                    self.stepIndex = 0
+                    # Jump
+                    self.jump = False
 
-            # inicia o player
-            pygame.mixer.init(44100, -16, 2, 1024)
+                def move_hero(self, userInput):
+                    if userInput[pygame.K_RIGHT] and self.x <= larguraTela - radius:
+                        self.x += self.velx
+                        self.face_right = True
+                        self.face_left = False
+                    elif userInput[pygame.K_LEFT] and self.x >= 0:
+                        self.x -= self.velx
+                        self.face_right = False
+                        self.face_left = True
+                    else:
+                        self.stepIndex = 0
 
-            # configura o volume da música
-            pygame.mixer.music.set_volume(0.8)
+                def draw(self, tela):
+                    if self.stepIndex >= 16:
+                        self.stepIndex = 0
+                    if self.face_left:
+                        tela.blit(left[self.stepIndex//4], (self.x, self.y))
+                        self.stepIndex += 1
+                    if self.face_right:
+                        tela.blit(right[self.stepIndex//4], (self.x, self.y))
+                        self.stepIndex += 1
 
-            while True:
-                for event in pygame.event.get():
-                    # se ele capturar um evento de fechar a tela (clicar no botao "X" no topo da janela), o programa é fechado
-                    if event.type == QUIT:
-                        exit()
-                    elif event.type == MOUSEMOTION:
-                        x_pos, y_pos = pygame.mouse.get_pos()
-                    elif event.type == MOUSEBUTTONDOWN:
-                        x_clique, y_clique = pygame.mouse.get_pos()
+                def jump_motion(self, userInput):
+                    if userInput[pygame.K_SPACE] and self.jump is False:
+                        self.jump = True
+                    if self.jump:
+                        self.y -= self.vely*2
+                        self.vely -= 1
+                    if self.vely < -15:
+                        self.jump = False
+                        self.vely = 15
 
-                posicao = (x_pos - 50, y_pos - 50)
+            # INIMIGO ----------------------------------------------------------------------------------------
+            left_enemy = [pygame.image.load(os.path.join('Inimigos', 'T1.png')),
+                        pygame.image.load(os.path.join('Inimigos', 'T2.png')),
+                        pygame.image.load(os.path.join('Inimigos', 'T3.png')),
+                        pygame.image.load(os.path.join('Inimigos', 'T4.png'))
+                        ]
+            right_enemy = [pygame.image.load(os.path.join('Inimigos', 'F1.png')),
+                        pygame.image.load(os.path.join('Inimigos', 'F2.png')),
+                        pygame.image.load(os.path.join('Inimigos', 'F3.png')),
+                        pygame.image.load(os.path.join('Inimigos', 'F4.png'))
+                        ]
 
-                x_duck += 1
+            class Enemy:
+                def __init__(self, x, y, direction):
+                    self.x = x
+                    self.y = y
+                    self.direction = direction
+                    self.stepIndex = 0
 
-                if x_duck * velocidade > 890 and not errou:
-                    x_duck = 0
-                    y_duck = randint(0, 450)
+                def step(self):
+                    if self.stepIndex >= 32:
+                        self.stepIndex = 0
 
-                    # errou, toca a música do game over
-                    pygame.mixer.music.load("gameover.mp3")
-                    pygame.mixer.music.play()
+                def draw(self, tela):
+                    self.step()
+                    if self.direction == left:
+                        tela.blit(left_enemy[self.stepIndex//8], (self.x, self.y))
+                    if self.direction == right:
+                        tela.blit(right_enemy[self.stepIndex // 8], (self.x, self.y))
+                    self.stepIndex += 1
 
-                    errou = True
+                def move(self):
+                    if self.direction == left:
+                        self.x -= 2
+                    if self.direction == right:
+                        self.x += 2
 
-                # colore o fundo de preto, deve sempre vir antes de desenharmos os objetos, no caso o circulo, pois se nao, tudo vai ficar preto!
-                screen.fill(preto)
-                pygame.mouse.set_visible(False)
+                def off_screen(self):
+                    return not(self.x >= -80 and self.x <= larguraTela + 30)
 
-                # o blit do background deve vir anter do blit da mira, pois assim o background sera desenhado
-                # primeiro e a mira depois, ficando o background atras da mira e nao o contrario!
-                screen.blit(pygame.image.load("background.png"), (0, 0))
-                screen.blit(pygame.font.SysFont("tahoma", 72).render("Pontos: " + str(pontos), True, branco), (500, 450))
-
-                # para serem contabilizados pontos, a posicao do clique deve estar mais ou menos onde o pato esta exatamente,
-                # por conta desse mais ou menos usamos a funcao range
-                if x_clique in range(x_duck * velocidade - 30, x_duck * velocidade + 30) and y_clique in range(y_duck - 30, y_duck + 30):
-                    # acertou, toca a musica do acerto
-                    pygame.mixer.music.load("hit.mp3")
-                    pygame.mixer.music.play()
-
-                    pontos += 1
-                    velocidade += 1
-                    x_duck = 0
-                    y_duck = randint(50, 500)
-
-                screen.blit(pygame.image.load("greenduck.gif"), (x_duck * velocidade, y_duck))
-
-                if errou:
-                    x_duck = -50
-                    y_duck = -50
-                    screen.blit(pygame.image.load("dog.gif"), (400, 340))
-
-                screen.blit(pygame.image.load("mira.gif").convert(), posicao)
-
+            # # # FUNÇÃO TELA ------------------------------------------------------------------------------
+            def draw_game():
+                tela.fill(BLACK)
+                tela.blit(background, (0, 0))
+                player.draw(tela)
+                for enemy in enemies:
+                    enemy.draw(tela)
+                # TEMPORIZADOR
+                tela.blit(texto, pos_texto)
+                #ATUALIZAÇÃO DA TELA
                 pygame.display.update()
-                #FINAL
+                CLOCK.tick(FPS)
+
+            player = Hero(250, 410)
+
+            enemies = []
+
+            # # # LOOP ------------------------------------------------------------------------
+            run = True
+            while run:
+
+                # # # FECHAR TELA ------------------------------------------------------------
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        run = False
+
+                #Input
+                userInput = pygame.key.get_pressed()
+
+                # Movement
+                player.move_hero(userInput)
+                player.jump_motion(userInput)
+
+                # Draw game in windows
+                draw_game()
+
+            # # # CONTROLE INIMIGOS
+                if len(enemies) == 0:
+                    rand_nr = random.randint(0, 1)
+                    if rand_nr == 1:
+                        enemy = Enemy(1010, 407, left)
+                        enemies.append(enemy)
+                    if rand_nr == 0:
+                        enemy = Enemy(-10, 407, right)
+                        enemies.append(enemy)
+                for enemy in enemies:
+                    enemy.move()
+                    if enemy.off_screen():
+                        enemies.remove(enemy)
+
+            # # # TEMPORIZADOR
+                if (timer <20): #tempo
+                    timer += 1
+                else: #tempo
+                    tempo_segundo +=1 #tempo
+                    texto = font.render("Tempo: " +str(tempo_segundo), True, (255, 0, 255), (1, 1,500))   #tempo
+
+                    timer = 0 #tempo
+#FIM
 
 
     def check_events(self):
